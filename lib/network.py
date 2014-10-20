d7e6761370d5528d4bcf7a8b0d2c0b392db9bcf0
foxhead128 <foxhead128@gmail.com>
2014-10-20 12:48:55 -0400

    Cleaner URL handling. APT url support added.

--- lib/network.py
+++ lib/network.py
@@ -119,14 +119,17 @@ class NetworkAccessManager(QNetworkAccessManager):
             html = directoryView % {"title": urlString, "heading": url.path(), "links": "".join(["<a href=\"%s\">%s</a><br/>" % (QUrl.fromUserInput(os.path.join(urlString, path)).toString(), path,) for path in [".."] + sorted(os.listdir(os.path.abspath(url.path())))])}
             return NetworkReply(self, url, self.GetOperation, html)
         if url.scheme() == "nimbus-extension":
-            request.setUrl(QUrl(request.url().toString().replace("nimbus-extension://", "http://127.0.0.1:8133/")))
+            request.setUrl(QUrl("http://127.0.0.1:8133/" + url.toString(QUrl.RemoveScheme)))
             return QNetworkAccessManager.createRequest(self, op, request, device)
         if url.scheme() == "nimbus":
-            request.setUrl(QUrl(request.url().toString().replace("nimbus://", "file://%s/" % (common.app_folder,))))
+            request.setUrl(QUrl("file://%s/" % (common.app_folder,) + url.toString(QUrl.RemoveScheme)))
             return self.createRequest(op, request, device)
         if url.scheme() == "nimbus-settings":
-            request.setUrl(QUrl(request.url().toString().replace("nimbus-settings://", "file://%s/" % (settings.settings_folder,))))
+            request.setUrl(QUrl("file://%s/" % (settings.settings_folder,) + url.toString(QUrl.RemoveScheme)))
             return self.createRequest(op, request, device)
+        if url.scheme() == "apt":
+            os.system("xterm -e \"sudo apt-get install %s\" &" % (url.toString(QUrl.RemoveScheme),))
+            return QNetworkAccessManager.createRequest(self, self.GetOperation, QNetworkRequest(QUrl("data:image/gif;base64,R0lGODlhAQABAHAAACH5BAUAAAAALAAAAAABAAEAAAICRAEAOw==")))
         if x != None or y or z or aa:
             return QNetworkAccessManager.createRequest(self, self.GetOperation, QNetworkRequest(QUrl("data:image/gif;base64,R0lGODlhAQABAHAAACH5BAUAAAAALAAAAAABAAEAAAICRAEAOw==")))
         else:
