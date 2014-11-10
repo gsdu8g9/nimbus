a878345d9259146af2c4daf3227a12deda904a67
foxhead128 <foxhead128@gmail.com>
2014-11-10 12:31:29 -0500

    Bumped to 1.5.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import subprocess
 import shutil
 from setuptools import setup
 
-version = "1.5.2"
+version = "1.5.3"
 applications_folder = os.path.join("/", "usr", "share", "applications")
 app_icon = os.path.join("lib", "icons", "%s", "nimbus.png")
 try: f = open(os.path.join("lib", "version.txt"), "w")
